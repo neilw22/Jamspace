687f0b7059e63c7cc305142328fe94f61ada0087
neilw22 <neilw22@gmail.com>
2016-01-22 12:48:24 -0500

    Updated photos, links, removed subscribe  and contact option for the moment.  Ready to go live for Jan 2016

--- js/custom.js
+++ js/custom.js
@@ -9,7 +9,7 @@ jQuery(function($) {
   // Create a countdown instance. Change the launchDay according to your needs.
   // The month ranges from 0 to 11. I specify the month from 1 to 12 and manually subtract the 1.
   // Thus the launchDay below denotes 7 May, 2014.
-  var launchDay = new Date(2015, 10-1, 14);
+  var launchDay = new Date(2016, 7-1, 19);
   $('#timer').countdown({
   until: launchDay
   });
@@ -36,9 +36,13 @@ jQuery(function($) {
     Bactstretch js
 =============================================== */
       $.backstretch([
-          "images/bx-slider/bg2.jpg",
-          "images/bx-slider/bg1.jpg",
-          "images/bx-slider/bg3.jpg"
+          "images/bx-slider/jam1.jpg",
+          "images/bx-slider/jam2.jpg",
+          "images/bx-slider/jam3.jpg",
+          "images/bx-slider/jam4.jpg",
+          "images/bx-slider/jam5.jpg",
+          "images/bx-slider/jam6.jpg",
+          "images/bx-slider/jam7.jpg"
         ], {
             fade: 750,
             duration: 4000
